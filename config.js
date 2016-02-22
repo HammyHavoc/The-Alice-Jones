be619f723df6ad7c1a9d586c502af4eee3a0c0c7
Hammy Havoc <hammy@previousmagazine.com>
2016-02-22 13:17:10 +0000

    Initial commit.

---
+++ config.js
@@ -0,0 +1,17 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'alice-jones-android',
+		wp_ws_url : 'http://www.thealicejones.com/wp-appkit-api/alice-jones-android',
+		theme : 'wpak-tabs-android',
+		version : '1.0',
+		app_title : 'The Alice Jones',
+		debug_mode : 'off',
+		auth_key : 'm^JK1 -Z2,P)S|5hv}V2E_8Dj{c9|~*mmf%XW-kwiQt%Tak+ub8&]XRfFDP;Oatf',
+		options : {"refresh_interval":0},
+		addons : []
+	};
+
+});
